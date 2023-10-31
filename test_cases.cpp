7f4332993e4b941f8933ac9193e7954c01fe722c
nehajag1 <116677708+nehajag1@users.noreply.github.com>
2023-10-31 11:31:11 -0500

    uploaded test cases & datasets

---
+++ test_cases.cpp
@@ -0,0 +1,35 @@
+#include "human.txt"
+#include <sstream>
+
+TEST_CASE("Testing countOccurrences on Human Data", "[weight=5]")
+{
+
+	std::string sequence = "CATTCTAT";
+    std::string infile = "C:\Users\nejag\OneDrive\Desktop\Extra Credit\human.txt"
+    int expected = 21;
+    buildSuffixArray(infile);
+    int out = countOccurrences(sequence);
+    REQUIRE(expected == out);
+}
+
+
+TEST_CASE("Testing findFirstOccurence on Dog Data", "[weight=5]")
+{
+
+	std::string sequence = "AGCTAGATACAT";
+    std::string infile = "C:\Users\nejag\OneDrive\Desktop\Extra Credit\human.txt"
+    int expected = 7;
+    buildSuffixArray(infile);
+    int out = findFirstOccurrence(sequence);
+    REQUIRE(expected == out);
+}
+
+TEST_CASE("Testing findLastOccurrence on Chimpanzee Data", "[weight=5]")
+{
+	std::string sequence = "CATTCTAT";
+    std::string infile = "C:\Users\nejag\OneDrive\Desktop\Extra Credit\human.txt"
+    int expected = 21;
+    buildSuffixArray(infile)
+    int out = findLastOccurrence(sequence);
+    REQUIRE(expected == out);
+}
