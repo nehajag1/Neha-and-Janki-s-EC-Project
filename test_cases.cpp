56aad785c3074d22a703e72164d20f69777359f6
nehajag1 <116677708+nehajag1@users.noreply.github.com>
2023-10-31 19:03:18 -0500

    Update test_cases.cpp

--- test_cases.cpp
+++ test_cases.cpp
@@ -17,7 +17,7 @@ TEST_CASE("Testing findFirstOccurence on Dog Data", "[weight=5]")
 {
 
 	std::string sequence = "AGCTAGATACAT";
-    std::string infile = "C:\Users\nejag\OneDrive\Desktop\Extra Credit\human.txt"
+    std::string infile = "C:\Users\nejag\OneDrive\Desktop\Extra Credit\dog.txt"
     int expected = 7;
     buildSuffixArray(infile);
     int out = findFirstOccurrence(sequence);
@@ -26,9 +26,9 @@ TEST_CASE("Testing findFirstOccurence on Dog Data", "[weight=5]")
 
 TEST_CASE("Testing findLastOccurrence on Chimpanzee Data", "[weight=5]")
 {
-	std::string sequence = "CATTCTAT";
-    std::string infile = "C:\Users\nejag\OneDrive\Desktop\Extra Credit\human.txt"
-    int expected = 21;
+	std::string sequence = "TACCCCCATACTCCTGACACTATTTCTCGT";
+    std::string infile = "C:\Users\nejag\OneDrive\Desktop\Extra Credit\chimpanzee.txt"
+    int expected = 64;
     buildSuffixArray(infile)
     int out = findLastOccurrence(sequence);
     REQUIRE(expected == out);
